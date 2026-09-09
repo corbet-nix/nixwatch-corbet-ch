ecad673db06857c48bf5ffb754ac6f383b49f1a1
Julian Y. Richard Corbet <48553615+julian-corbet@users.noreply.github.com>
2026-09-09 17:08:22 +0200

    ci: use Git transport for nixk3s input [skip ci]

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     };
 
     nixk3s = {
-      url = "github:julian-corbet/nixk3s-corbet-ch";
+      url = "git+https://github.com/julian-corbet/nixk3s-corbet-ch";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixidy.follows = "nixidy";
     };
